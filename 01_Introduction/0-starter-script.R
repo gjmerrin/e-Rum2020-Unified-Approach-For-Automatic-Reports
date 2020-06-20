be3829d840caf4d45c06cf566fa197471c44b5ea
Joey Merrin <gjmerrin@gmail.com>
2020-06-20 03:21:17 -0500

    install package script
    add notes

--- 01_Introduction/0-starter-script.R
+++ 01_Introduction/0-starter-script.R
@@ -2,5 +2,7 @@
 pacman::p_load("yaml", "data.table", "stringr", "readr", "config", "ggthemes",
                 "ggplot2", "prettydoc", "rmarkdown", "scales", "tidyverse", "dplyr",
                 "kableExtra", "RCurl", "EQL", "kdensity")
+
+# this wont work because it is on the presenters local computer. Use tools --> install packages and select gs file
 install.packages("05_PrescRiptions_project/packages/PrescRiptions_0.2.5.tar.gz", repos = NULL, type = "source")
-install.packages("01_Introduction/packages/PrescRiptions_0.2.5.tar.gz", repos = NULL, type = "source")
+
