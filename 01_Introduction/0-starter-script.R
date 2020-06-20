c1f12b1f4a704e24659323358fccece4757747ac
Joey Merrin <gjmerrin@gmail.com>
2020-06-20 03:12:03 -0500

    starter script
    added install packages to file

---
+++ 01_Introduction/0-starter-script.R
@@ -0,0 +1,6 @@
+
+pacman::p_load("yaml", "data.table", "stringr", "readr", "config", "ggthemes",
+                "ggplot2", "prettydoc", "rmarkdown", "scales", "tidyverse", "dplyr",
+                "kableExtra", "RCurl", "EQL", "kdensity")
+install.packages("05_PrescRiptions_project/packages/PrescRiptions_0.2.5.tar.gz", repos = NULL, type = "source")
+install.packages("01_Introduction/packages/PrescRiptions_0.2.5.tar.gz", repos = NULL, type = "source")
